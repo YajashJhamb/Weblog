585c926947e547dd00fcec6ea7c59e081d35e869
yajash <yajash989@gmail.com>
2019-03-14 16:59:48 +0530

    added google oAuth

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+const passport = require("passport");
 
+//Passport Config
+require("./config/passport")(passport);
+
+//Load Routes
 const app = express();
+
 app.get("/", (re, res) => {
   res.send("hello");
 });
+
+//Use Routes
+app.use("/auth", auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://yajash:yajash123@ds127545.mlab.com:27545/weblog_dev",
+  googleClientID:
+    "157464219251-1654n9gsojn8sf9tuimcpdtups704aak.apps.googleusercontent.com",
+  googleClientSecret: "TzKEuOGEppyeVhAyckh8LRbc"
+};
